b5462655a04d930633dd69ff4ef8b05be98d4137
root <root@centos7.dual.edu>
2022-05-03 13:02:07 +0200

    Uploading project done by tutorial

---
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -0,0 +1,100 @@
+package com.example.demo.appuser;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.lang.*;
+import java.util.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+public class AppUser implements UserDetails {
+
+
+
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
+
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole;
+    private Boolean locked = false;
+    private Boolean enabled = false;
+
+    public AppUser(String firstName, String lastName, String email, String password, AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+
+    }
+
+
+    @java.lang.Override
+    public java.util.Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singletonList(authority);
+    }
+
+    @java.lang.Override
+    public java.lang.String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public java.lang.String getLastName() {
+
+        return lastName;
+    }
+
+    @java.lang.Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @java.lang.Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @java.lang.Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @java.lang.Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
---
+++ src/main/java/com/example/demo/registration/EmailValidator.java
@@ -0,0 +1,12 @@
+package com.example.demo.registration;
+
+import java.util.function.Predicate;
+
+public class EmailValidator implements Predicate<String> {
+
+
+    @Override
+    public boolean test(String s) {
+        return true;
+    }
+}
